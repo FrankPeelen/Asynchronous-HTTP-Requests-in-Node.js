fd64838ecab8510d23313040df8333fb5591837b
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-05 14:25:55 +0900

    Asynchronize

--- client.js
+++ client.js
@@ -1,19 +1,28 @@
 'use strict';
 
 var request = require('request');
+var async = require('async');
 
-request.get(
-	'http://localhost:8080/getUserName?id=1234',
-	function(err, res, body) {
-		var result = JSON.parse(body);
-		var name = result.value;
+var name, status;
 
-		request.get(
-			'http://localhost:8080/getUserStatus?id=1234',
-			function(err, res, body) {
-				var result = JSON.parse(body);
-				var status = result.value;
-				console.log('The status of user ', name, ' is ', status);
-			});
-	}
-);
+var getUsername = function(callback) {
+	request.get(
+		'http://localhost:8080/getUserName?id=1234',
+		function(err, res, body) {
+			var result = JSON.parse(body);
+			callback(err, result.value);
+		});
+};
+
+var getUserStatus = function(callback) {
+	request.get(
+		'http://localhost:8080/getUserStatus?id=1234',
+		function(err, res, body) {
+			var result = JSON.parse(body);
+			callback(err, result.value);
+		});
+};
+
+async.parallel([getUsername, getUserStatus], function(err, results) {
+	console.log('The status of user ', results[0], ' is ', results[1]);
+});
